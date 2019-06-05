ffb078adac0d99e175e4a9ccbd046e0aac1bd6dd
Adam <adam@Adams-MacBook-Air.local>
2019-06-05 13:13:11 +0100

    updated transition for external link

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -5,8 +5,7 @@ import theme from '../theme';
 const ExternalLink = styled.a `
   text-decoration: none;
   color: inherit;
-  // padding-bottom: 1px;
-  transition: background-color 0.25s;
+  transition: background-color 0.25s, border-bottom 0.25s;
   : hover {
     background-color: ${theme.colors.lightGreen};
     border-bottom: 2px black solid;
